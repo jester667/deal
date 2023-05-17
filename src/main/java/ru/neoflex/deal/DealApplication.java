e4d2ef4fcc2453a485e5ade5c43f798fce424fea
Томко Владислав Викторович <tomko@dev.vtb.ru>
2023-05-17 15:45:10 +0300

    add local library audit-producer

--- src/main/java/ru/neoflex/deal/DealApplication.java
+++ src/main/java/ru/neoflex/deal/DealApplication.java
@@ -3,9 +3,11 @@ package ru.neoflex.deal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan({"ru.neoflex.deal", "ru.neoflex.auditproducer"})
 public class DealApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/neoflex/deal/controller/DealController.java
+++ src/main/java/ru/neoflex/deal/controller/DealController.java
@@ -3,6 +3,7 @@ package ru.neoflex.deal.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.neoflex.auditproducer.aop.AuditAction;
 import ru.neoflex.deal.model.FinishRegistrationRequestDTO;
 import ru.neoflex.deal.model.LoanApplicationRequestDTO;
 import ru.neoflex.deal.model.LoanOfferDTO;
@@ -16,35 +17,42 @@ import java.util.List;
 public class DealController {
 
     private final DealService dealService;
+    private static final String DEAL = "DEAL";
 
     @PostMapping("/application")
+    @AuditAction(auditService = DEAL)
     public ResponseEntity<List<LoanOfferDTO>> application(@RequestBody LoanApplicationRequestDTO req) {
         return ResponseEntity.ok(dealService.createApplication(req));
     }
 
     @PutMapping("/offer")
+    @AuditAction(auditService = DEAL)
     public ResponseEntity<Void> offer(@RequestBody LoanOfferDTO req) {
         dealService.acceptOffer(req);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/calculate/{applicationId}")
+    @AuditAction(auditService = DEAL)
     public ResponseEntity<Void> calculate(@PathVariable Long applicationId, @RequestBody FinishRegistrationRequestDTO req) {
         dealService.calculate(applicationId, req);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/document/{applicationId}/send")
+    @AuditAction(auditService = DEAL)
     public ResponseEntity<Void> send(@PathVariable Long applicationId) {
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/document/{applicationId}/sign")
+    @AuditAction(auditService = DEAL)
     public ResponseEntity<Void> sign(@PathVariable Long applicationId) {
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/document/{applicationId}/code")
+    @AuditAction(auditService = DEAL)
     public ResponseEntity<Void> code(@PathVariable Long applicationId) {
         return ResponseEntity.ok().build();
     }
